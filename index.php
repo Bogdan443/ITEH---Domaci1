939aeb67baa83e9a82909bd1bbeb83d9cadd5895
Bogdan Savic <bs20201086@student.fon.bg.ac.rs>
2023-05-08 10:22:56 +0200

    Inicijalizacija HTML i CSS

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/fdf8cea92a.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" type="image/png" href="img/Favicon.png">
+    <link rel="stylesheet" href="index.css">
+    <title>LiquorStoreRS - Kuća dobrog pića</title>
+    
+</head>
+<body>
+
+    
+
+
+
+    <div class="container">
+
+
+      
+    </div>
+
+
+        <!-- navigacija -->
+
+        <div class="container-nav">
+
+            <nav>
+                <ul class="list">
+                    <p></p>
+                    <li class="link"><a href=""><img src="img/logo.png" alt=""></a></li>
+                    <p></p>
+                    <li class="link"><a href="index.php" class="abc">Početna</a></li>
+                    <li class="link"><a href="about.html" class="abc">O nama</a></li>
+                    <li class="link"><a href="katalog.html" class="abc">Katalog</a></li>
+                    <li class="link"><a href="news.html" class="abc">Novosti</a></li>
+                    <li class="link"><a href="contact.html" class="abc">Kontakt</a></li>
+                    <p></p>
+                    <p></p>
+                <li class="link"><a href="login.php"><div class="user"><i class="fa-solid fa-user"> </i></div></a></li>
+                    <p></p>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="line"></div>
+        
+        <div class="container7">
+
+
+          <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active" data-bs-interval="10000">
+                <img src="img/3.jpeg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Naizgled običan whiskey...</h5>
+                  <p>Mozda na prvi pogled jedna manje više obična flaša žestine u moru istih, ali izgled zna da prevari. U pitanju je škotski prvak Chivas star 15 godina. Potraži u našem katalogu.</p>
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="2000">
+                <img src="img/1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="img/2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Third slide label</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        </div>
+
+        <div class="line"></div>
+
+        <div class="line1"></div>
+        
+
+          
+
+          <div class="container1">
+            <p></p>
+            <div class="box1">
+                <a href=""><img src="img/112.png" alt=""></a>
+                
+            </div>
+            <div class="box1">
+                <a href=""><img src="img/111.png" alt=""></a>
+                
+            </div>
+            <div class="box1">
+              <a href=""><img src="img/113.png" alt=""></a>
+              
+          </div>
+            <p></p>
+          </div>
+
+          <div class="container1">
+            <p></p>
+            <div class="box1">
+                <a href=""><h3>Cognac</h3></a>
+                
+            </div>
+            <div class="box1">
+                <a href=""><h3>Whiskey</h3></a>
+                
+            </div>
+            <div class="box1">
+              <a href=""><h3>Wine</h3></a>
+              
+          </div>
+            <p></p>
+          </div>
+
+          <div class="line"></div>
+
+          <div class="container10">
+            <p></p>
+            <div class="box10"><img src="img/jamison.png" alt=""></div>
+            <div class="box11">
+              <h2>U našoj ponudi pića nalazi se preko 80 napitaka</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum eligendi voluptatum tempore aspernatur temporibus nulla, ipsum natus. Nihil quas tempore voluptas sequi odio reprehenderit! Officiis odio itaque voluptatibus? Minus, fugit blanditiis est illo officiis reiciendis excepturi maxime asperiores atque iure.</p>
+            </div>
+            <p></p>
+          </div>
+
+          <div class="line"></div>
+
+
+          <div class="container4">
+            <p></p>
+            <div class="box5">
+              <p><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11321.188162967343!2d20.4360662!3d44.8155129!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd9fb62470be09800!2z0KPQqNCL0JUg0KjQvtC_0LjQvdCzINCm0LXQvdGC0LDRgA!5e0!3m2!1ssr!2srs!4v1673216066498!5m2!1ssr!2srs" width="1100" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>
+              <p>Naša radnja nalazi se na drugom spratu tržnog centra Ušće, na Novom Beogradu, u bulevaru Mihajla Pupina, broj 4. Kontakt 011/2345-678</p>
+            </div>
+              <p></p>
+            </div>
+         
+          
+
+          <div class="line"></div>
+
+          <footer>
+            <p>&reg LiquorStore Serbia 2022 - All rights reserved </p>
+          </footer>
+        
+          
+</body>
+</html>
